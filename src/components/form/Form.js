408ab993f12d8aa5cbf9ccc42a416e0a6febfb65
nazmiyegnydnn <nazmiyeyilmaz159@gmail.com>
2023-07-27 15:56:46 +0300

    Todo  add and delete completedd

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,19 +1,44 @@
 import React from 'react'
+import './Form.scss'
 
-const Form = () => {
+const Form = ({
+  inputText,
+  setInputText,
+  todos={todos},
+  setTodos={setTodos}
+}) => {
+
+const inputTextHandler = (e) => {
+  console.log(e.target.value)
+  setInputText(e.target.value)
+}
+const submitTodoHandler = (e) => {
+  e.preventDefault();
+  setInputText("")
+  setTodos([
+   ...todos,
+   {text: inputText , completed:false , id:Math.random() }
+  ]);
+  console.log(todos)
+ }
+
+
   return (
     <div className='app'>
       <form>
         <div className='search'>
-            <input type='text' className='todo-input' placeholder='Add...'/>
-            <button className='todo-button' type='submit'>
-                <i className='fas fa-plus-circle'></i>
-            </button>
+            <input type='text' 
+            className='todo-input' 
+            placeholder='Add...'
+            onChange={inputTextHandler}
+            value={inputText}
+            />
+            <button className='todo-button' type='submit' onClick={submitTodoHandler}>ADD</button>
         
         <div className='select'>
             <select name='todos' className='filter-todo'>
                 <option value="all">All</option>
-                <option value="completed">Comleted</option>
+                <option value="completed" className='completed'>Comleted</option>
                 <option value="uncompleted">Uncompleted</option>
             </select>
         </div>
